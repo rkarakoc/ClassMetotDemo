c2164630f2e6ff6d5cd734898b52fb8231a1eae4
rıza karakoç <rizakarakoc@gmail.com>
2021-01-25 16:56:42 +0300

    Add project files.

---
+++ HomeWork/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork
+{
+    public class CustomerManager
+    {
+        
+        public void Add(Customers customer)
+        {
+            Console.WriteLine(customer.Id + "Id nolu müşteri eklendi.");
+        }
+        public void Update(Customers customer)
+        {
+            Console.WriteLine(customer.Id + "Id nolu müşteri güncellendi.");
+        }
+        public void Delete(Customers customer)
+        {
+            Console.WriteLine(customer.Id + "Id nolu müşteri silindi.");
+        }
+       
+        public void List(Customers[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Id);
+                Console.WriteLine(customer.FirstName);
+                Console.WriteLine(customer.LastName);
+
+            }
+        }
+    }
+}
---
+++ HomeWork/Program.cs
@@ -0,0 +1,37 @@
+﻿using Stripe;
+using System;
+using System.Collections.Generic;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Customers customer1 = new Customers();
+            customer1.Id = 1;
+            customer1.TcNo = "12345678990";
+            customer1.FirstName = "Rıza";
+            customer1.LastName = "Karakoç";
+            customer1.PhoneNumber = "5511234567";
+            customer1.Mail = "rizakarakoc@karakoc.com";
+            customer1.City = "Balıkesir";
+
+            Customers customer2 = new Customers();
+            customer2.Id = 2;
+            customer2.TcNo = "12345678992";
+            customer2.FirstName = "Pınar";
+            customer2.LastName = "Karakoç";
+            customer2.PhoneNumber = "5551234567";
+            customer2.Mail = "pinarkarakoc@karakoc.com";
+            customer2.City = "Balıkesir";
+            Customers[] customers = new Customers[] { customer1, customer2 };
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            
+            customerManager.List(customers);
+
+        }
+    }
+}
